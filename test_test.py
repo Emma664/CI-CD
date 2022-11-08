840bdb97cdc39d3f0c0c52b6f4d076afd2f1adfa
Volha_Bykhautsova <volha_bykhautsova@epam.com>
2022-11-08 16:40:11 +0100

    add jenkins file

--- test_test.py
+++ test_test.py
@@ -1,8 +1,8 @@
 import pymssql
 
 from checker import *
-conn = pymssql.connect(server='127.0.0.1', user='youruser', password='yourpassword',
-                       database='AdventureWorks2014', port='yourport')
+conn = pymssql.connect(server='127.0.0.1', user='root', password='MegaPass123@',
+                       database='AdventureWorks2014', port='52797')
 
 
 def test_column_completeness_addressline1_address():
